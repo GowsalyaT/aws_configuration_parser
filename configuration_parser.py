ff0fb9f1c2e65dab8708723a29c0bab0f4ccf4a9
litchyT <kowsaalya@gmail.com>
2017-08-29 02:04:11 -0400

    Version2

--- configuration_parser.py
+++ configuration_parser.py
@@ -1,12 +1,24 @@
 import configparser
 import os
-Header = input("Enter the header value : ")
+import subprocess
+import sys
+from subprocess import Popen, PIPE
 
+### Fetching and assigning the command line argument  ###
+profileName = sys.argv[1]
+
+### Exception Handling ###
+try:
+    print(subprocess.check_output(['./test.sh', 'test'], shell=True))
+except Exception as e:
+    print(e)
+
+### Configuration parser ###
 config = configparser.ConfigParser()
 config.read("config.ini")
-aws_access_key_id = config.get(Header, "aws_access_key_id")
-aws_secret_access_key = config.get(Header, "aws_secret_access_key")
-aws_session_token = config.get(Header, "aws_session_token")
+aws_access_key_id = config.get(profileName, "aws_access_key_id")
+aws_secret_access_key = config.get(profileName, "aws_secret_access_key")
+aws_session_token = config.get(profileName, "aws_session_token")
 os.environ["AWS_ACCESS_KEY_ID"] = aws_access_key_id
 os.environ["AWS_SECRET_ACCESS_KEY"] = aws_secret_access_key
 os.environ["AWS_SESSION_TOKEN"] = aws_session_token
